ce01c3ad07d910727ab9955083e2ff7dc5871106
thienhuynhhh <bth@ualberta.ca>
2023-11-30 03:16:19 -0700

    first commit

---
+++ sockets.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) 2013-2014 Abram Hindle
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Name or identifier: Ba Thien Huynh
+# License: MIT License
+
+
+
+# MIT License
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE
+
+import flask
+from flask import Flask, request,redirect
+from flask_sockets import Sockets
+from flask_sock import Sock
+import gevent
+from gevent import queue
+import time
+import json
+import os
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+
+app = Flask(__name__)
+sockets = Sockets(app)
+app.debug = True
+
+class World:
+    def __init__(self):
+        self.clear()
+        # we've got listeners now!
+        self.listeners = list()
+        
+    def add_set_listener(self, listener):
+        self.listeners.append( listener )
+
+    def update(self, entity, key, value):
+        entry = self.space.get(entity,dict())
+        entry[key] = value
+        self.space[entity] = entry
+        self.update_listeners( entity )
+
+    def set(self, entity, data):
+        self.space[entity] = data
+        self.update_listeners( entity )
+
+    def update_listeners(self, entity):
+        '''update the set listeners'''
+        for listener in self.listeners:
+            listener(entity, self.get(entity))
+
+    def clear(self):
+        self.space = dict()
+
+    def get(self, entity):
+        return self.space.get(entity,dict())
+    
+    def world(self):
+        return self.space
+
+myWorld = World()        
+
+def set_listener( entity, data ):
+    ''' do something with the update ! '''
+    print(f"Received update for entity {entity}: {data}")
+
+myWorld.add_set_listener( set_listener )
+        
+@app.route('/')
+def hello():
+    '''Return something coherent here.. perhaps redirect to /static/index.html '''
+    return redirect("/static/index.html")
+
+def read_ws(ws,client):
+    '''A greenlet function that reads from the websocket and updates the world'''
+    while not ws.closed:
+        message=ws.receive()
+        message= json.loads(message)
+        entity=message.get("entity")
+        data=message.get("data")
+        if data=="hi":
+            json_data = json.dumps(myWorld.world())
+            ws.send(json_data)
+        elif data['x'] is not None and data['y'] is not None:
+            for key in data:
+                myWorld.update(entity,key,data[key])
+
+
+@sockets.route('/subscribe')
+def subscribe_socket(ws):
+    '''Fufill the websocket URL of /subscribe, every update notify the
+       websocket and read updates from the websocket '''
+    read_ws(ws, None)
+    return None
+
+
+
+# I give this to you, this is how you get the raw body/data portion of a post in flask
+# this should come with flask but whatever, it's not my project.
+def flask_post_json():
+    '''Ah the joys of frameworks! They do so much work for you
+       that they get in the way of sane operation!'''
+    if (request.json != None):
+        return request.json
+    elif (request.data != None and request.data.decode("utf8") != u''):
+        return json.loads(request.data.decode("utf8"))
+    else:
+        return json.loads(request.form.keys()[0])
+
+@app.route("/entity/<entity>", methods=['POST','PUT'])
+def update(entity):
+    '''update the entities via this interface'''
+    data=flask_post_json()
+    if data['x'] is not None and data['y'] is not None:
+        for key in data:
+            myWorld.update(entity,key,data[key])
+        return data,200
+    return {"message":"x and y coordinates are required"},400
+ 
+
+@app.route("/world", methods=['GET'])    
+def world():
+    '''you should probably return the world here'''
+    return myWorld.world()
+
+@app.route("/entity/<entity>", methods=['GET'])    
+def get_entity(entity):
+    '''This is the GET version of the entity interface, return a representation of the entity'''
+    return myWorld.get(entity)
+
+
+@app.route("/clear", methods=['POST'])
+def clear():
+    '''Clear the world out!'''
+    myWorld.clear()
+    return myWorld.world()
+
+
+
+if __name__ == "__main__":
+    ''' This doesn't work well anymore:
+        pip install gunicorn
+        and run
+        gunicorn -k flask_sockets.worker sockets:app
+    '''
+    app.run(port=8000)
